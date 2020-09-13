ff50766d9774631744c03e96e5b1aea0a7df8f4a
Calandrinon <georgesut@yahoo.com>
2020-09-13 16:53:55 +0300

    Shorten run() from console.py
    That method was over 50 lines of code...

--- console.py
+++ console.py
@@ -4,45 +4,16 @@ class Console:
     def __init__(self, controller):
         self.controller = controller
 
-    def run(self):
-        print("""
-   _____  .__ ___.                  __                .__        
-  /  _  \ |  |\_ |__  __ __  ______/  |_  ____ ______ |__|____   
- /  /_\  \|  | | __ \|  |  \/     \   __\/  _ \\____ \|  \__  \  
-/    |    \  |_| \_\ \  |  /  Y Y  \  | (  <_> )  |_> >  |/ __ \_
-\____|__  /____/___  /____/|__|_|  /__|  \____/|   __/|__(____  /
-        \/         \/            \/            |__|           \/ 
 
-
-                                      @R$k
-                                    '$!!!M&
-                                    @?~~~!$k
-                                  '9!!~ ~~!MX
-                                  @X~~   `~!$k
-                                 9!!      ~~!$X
-                                dR!~       `~!$>
-                               XR!~         `~!$k:
-                              tR!~        ::~~!!MMXXHHHH!!<:.
-                             <$!xxiXX!!!~~~~~~~!!MMMMMMMMMMMMMMMXXXXx::
-                        .:X@N$$$RMMX!!!!~~~~~~~~!!MMMM@@MMMMMMMMMMMMXMSMMtHHHX!
-                  :xiM#"~  <$!~  `~~~!~~~~~~~~~~~!!MX!!!!!??#RR888MMMMMMMMMMMHH
-           ..XH@!~`       X$!~                 '~~~!MX!!!!!!!!!!!!?MMR@@$MMMMMM
-     :xiM#"~             <$!~                     '~!MM??MMX!!!!!!!!!!!!!!??#R$
-XH@M!``                 :$!!                        ~!R:   `~!MM!XH!!!!!!!!!!!!
-                       <$!~                         `!!M:        `~"??HHX!!!!!!
-                      :$!~                           `!!R:              `!!MMMH
-                     '$!~                             `~!8:                   ~
-                    :$!!                               ~!!N:
-                   '$!~                                 `!!N:
-                  .$!!~                                  ~~!&>
-                 '@!!~                                    ~!MN
-                .$!!~                                     ~~!M&>
-               :@$MHHHHHHHHH!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!H$N
-        """)
+    def __display_banner(self): 
+        banner_file = open("res/banner.txt", "r")
+        banner_file_lines = banner_file.readlines()
+        for line in banner_file_lines:
+            print(line, end="")
         print("Welcome!")
-        band_name = input("Type the artist name: ")
-        album_title = input("Type the album title: ")
 
+
+    def __display_options_message(self):
         print("Choose one of the following options: ")
         print("1. Download a full album as an mp3 and split it in separate tracks")
         print("2. Download each track of an album")
@@ -50,6 +21,15 @@ XH@M!``                 :$!!                        ~!R:   `~!MM!XH!!!!!!!!!!!!
         print("Sometimes the separate tracks of the album cannot be found, but the full album in mp3 exists.")
         print("If you are dissatisfied with the result of an option, you can try the other.")
         print("In any case, you can create an issue at https://github.com/Calandrinon/Albumtopia/issues")
+
+
+    def run(self):
+        self.__display_banner()
+
+        band_name = input("Type the artist name: ")
+        album_title = input("Type the album title: ")
+
+        self.__display_options_message()
 
         option = int(input("Enter the option number: "))
         print("Be patient. It can last a while... (2 minutes, in the worst case)")
